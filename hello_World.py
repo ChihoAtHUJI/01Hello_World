2e1eff2467909e93f1f6540226a47ce00d468800
ChihoSong <songchiho777@gmail.com>
2020-06-27 08:29:15 +0900

    Update hello_World.py

--- hello_World.py
+++ hello_World.py
@@ -1,11 +1,1 @@
-
-def new():
-    if (a > 3):
-        print("hi")
-        
-    print("hi")
-
-
-if __name__ == '__main__':
-    
-    
+def new():    if (a > 3):        print("hi")            print("hi")    if __name__ == '__main__':

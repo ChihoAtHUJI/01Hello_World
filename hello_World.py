86ea101f99646e8f8a79542af7d14ef623c324c6
ChihoSong <songchiho777@gmail.com>
2020-06-27 23:49:26 +0900

    new_1(5)

--- hello_World.py
+++ hello_World.py
@@ -1,1 +1,1 @@
-def new():    print("hi")    print("hi")new()new()def new_1(a):    if (a >3):        print("it is over 3")    else:        print("it it below 3")new_1(4)
+def new():    print("hi")    print("hi")new()new()def new_1(a):    if (a >3):        print("it is over 3")    else:        print("it it below 3")new_1(4)new_1(5)
